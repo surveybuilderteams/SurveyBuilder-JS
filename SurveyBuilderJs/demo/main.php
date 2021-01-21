0f3fe9109c8e245454a6bafe3e6dc017dd2ee466
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-01-21 07:06:04 -0500

    Add files via upload

--- SurveyBuilderJs/demo/main.php
+++ SurveyBuilderJs/demo/main.php
@@ -19,13 +19,13 @@ body{
 <body>
 
 <div class="renamed">
-
+<center id="Form-Center" onload="Autosave()"><form id="Editors-Form" method="post" action=""><div id="Insert-Object" contenteditable="true"></div></form>
 
 </div>
 <script type="text/javascript">
-let sb = new SurveyBuilder({
+ let sb = new SurveyBuilder({
    lang: "en",
-   sel: ".renamed",
+   sel: "#Insert-Object",
    public_key: "VmgzuAUN2hKjdr65Sxs3BOHQqeDXYlwfC0vMJypFkcoP9WGIL47nt81iRbTaEZ",
    privite_key: "Y0TkA$thxD4f5IldZj#aB?Wy*Hic%C8Ur3eMgRu9wvnON@So&b6^q2K7LFVJQX1mG!zEspP",
    full_setup: false,
@@ -39,7 +39,8 @@ let sb = new SurveyBuilder({
    scripts:[
        "/SurveyBuilder/Builder/Apps_Insert.js",
        "/SurveyBuilder/Builder/SurveyBuilder.js",
-       "/SurveyBuilder/Builder/InsertItems.js"
+       "/SurveyBuilder/Builder/InsertItems.js",
+	   "https://kit.fontawesome.com/46bb4793e2.js"
    ],
    el:[{
 	type: "heading",
@@ -49,6 +50,65 @@ let sb = new SurveyBuilder({
     classNames: "heading",
     val: "Hello World",
 	editable: true
+   },
+   {
+	type: "paragraph",
+	name: "MyPara",
+    id: "MyPara",
+    classNames: "para",
+    val: "Hello World",
+	editable: true  
+   },
+   {
+	type: "name",
+	name: "input",
+	id: "MyName",
+	classNames: "Name",
+	editable: false,
+	required: true
+   },
+   {
+	  type: "email",
+	name: "input",
+	id: "MyEmail",
+	classNames: "Email",
+	editable: false,
+	required: true 
+   },
+   {
+	    type: "address",
+	name: "selectElement",
+	id: "MyAddress",
+	classNames: "Address",
+	selectedIndex: 186,
+	editable: false,
+	required: true 
+   },
+   {
+	 type: "phone",
+	name: "input",
+	id: "MyPhone",
+	classNames: "Phone",
+	editable: false,
+	required: true   
+   },
+   {
+	    type: "date-picker",
+	name: "",
+	id: "MyDate",
+	val: "",
+	classNames: "Date",
+	editable: false,
+	required: true   
+   },
+   {
+	  type: "time-picker",
+	name: "",
+	id: "MyTime",
+	val: "22:00:00",
+	classNames: "Time",
+	editable: false,
+	required: true    
    }
   
   ]
@@ -56,6 +116,8 @@ let sb = new SurveyBuilder({
    
 });
 console.log(sb);
+
+
 </script>
 </body>
 </html>
